9922d91b3e59d5c374da516b474767a76918c6a8
tundeiness <tundeiness@yahoo.co.uk>
2019-09-02 17:01:32 +0100

    create session controller, create and destroy actions

---
+++ db/migrate/20190902155241_add_remember_token_to_users.rb
@@ -0,0 +1,8 @@
+class AddRememberTokenToUsers < ActiveRecord::Migration[5.2]
+  def change
+
+      add_column :users, :remember_token, :string
+      add_index  :users, :remember_token
+
+  end
+end
